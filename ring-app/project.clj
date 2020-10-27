e0450cf7fbc9d490d358e87016792a10afb1fa01
Joe Averbukh <joeaverbukh@gmail.com>
2020-10-27 01:56:12 -0400

    Add simple api

--- ring-app/project.clj
+++ ring-app/project.clj
@@ -4,6 +4,9 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
+                  [metosin/muuntaja "0.6.4"]
+                  [metosin/reitit "0.3.9"]
+                  [metosin/ring-http-response "0.9.1"]
                   [ring "1.7.1"]]
   :repl-options {:init-ns ring-app.core}
   :main ring-app.core)
--- ring-app/src/ring_app/core.clj
+++ ring-app/src/ring_app/core.clj
@@ -1,13 +1,10 @@
 (ns ring-app.core
-  (:require [ring.adapter.jetty :as jetty]
-            [ring.util.response :as response]
-            [ring.middleware.reload :refer [wrap-reload]]))
-
-(defn handler [request-map]
-  (response/response
-  (str "<html><body> your IP is:"
-              (:remote-addr request-map)
-              "</body></html>")))
+  (:require
+   [reitit.ring :as reitit]
+   [ring.adapter.jetty :as jetty]
+   [ring.util.http-response :as response]
+   [ring.middleware.reload :refer [wrap-reload]]
+   [muuntaja.middleware :as muuntaja]))
 
 (defn wrap-nocache [handler]
   (fn [request]
@@ -15,11 +12,36 @@
         handler
         (assoc-in [:headers "Pragma"] "no-cache"))))
 
+(defn wrap-formats [handler]
+  (-> handler
+      (muuntaja/wrap-format)))
+
+(defn response-handler [request-map]
+  (response/ok
+   (str "<html><body> your IP is:"
+        (:remote-addr request-map)
+        "</body></html>")))
+
+(def routes
+  [["/" {:get response-handler}]
+   ["/echo/:id"
+    {:get
+     (fn [{{:keys [id]} :path-params}]
+       (response/ok (str "<p>the value is: " id "</p>")))}]
+   ["/api" {:middleware [wrap-formats]}
+    ["/multiply"
+     {:post
+      (fn [{{:keys [a b]} :body-params}]
+        (response/ok {:result (* a b)}))}]]])
+
+(def handler
+  (reitit/ring-handler
+   (reitit/router routes)))
+
 (defn -main []
   (jetty/run-jetty
-    (-> handler
-        var
-        wrap-nocache
-        wrap-reload)
-    {:port 3000
-     :join? false}))
+   (-> #'handler
+       wrap-nocache
+       wrap-reload)
+   {:port 3000
+    :join? false}))
