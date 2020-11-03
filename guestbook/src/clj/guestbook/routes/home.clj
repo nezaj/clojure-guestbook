ca77df044c0e29d4c3ea511cb1993af8d8fdd6d1
Joe Averbukh <joeaverbukh@gmail.com>
2020-11-03 12:42:21 -0500

    Separate message logic into model and api

--- guestbook/src/clj/guestbook/routes/home.clj
+++ guestbook/src/clj/guestbook/routes/home.clj
@@ -1,13 +1,9 @@
 (ns guestbook.routes.home
   (:require
-   [guestbook.layout :as layout]
-   [guestbook.db.core :as db]
-   [clojure.java.io :as io]
-   [guestbook.middleware :as middleware]
    [ring.util.response]
-   [ring.util.http-response :as response]
-   [struct.core :as st]
-   [guestbook.validation :refer [validate-message]]))
+
+   [guestbook.layout :as layout]
+   [guestbook.middleware :as middleware]))
 
 (defn home-page [request]
   (layout/render request "home.html"))
@@ -15,28 +11,10 @@
 (defn about-page [request]
   (layout/render request "about.html"))
 
-(defn save-message! [{:keys [params]}]
-  (if-let [errors (validate-message params)]
-    (do
-      (println System/out "Oh noes...")
-      (response/bad-request {:errors errors}))
-    (try
-      (db/save-message! params)
-      (println System/out "Saved message!")
-      (response/ok {:status :ok})
-      (catch Exception _
-        (response/internal-server-error
-         {:errors {:server-error ["Failed to save message!"]}})))))
-
-(defn message-list [_]
-  (response/ok {:messages (vec (db/get-messages))}))
-
 (defn home-routes []
   [""
    {:middleware [middleware/wrap-csrf
                  middleware/wrap-formats]}
    ["/" {:get home-page}]
    ["/index.html" {:get home-page}]
-   ["/about" {:get about-page}]
-   ["/messages" {:get message-list}]
-   ["/message" {:post save-message!}]])
+   ["/about" {:get about-page}]])
