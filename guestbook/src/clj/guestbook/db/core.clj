b02a0ca061f0e069b3238c0081db27557385a709
Joe Averbukh <joeaverbukh@gmail.com>
2020-11-21 16:48:58 -0500

    Re-factor db queries

--- guestbook/src/clj/guestbook/db/core.clj
+++ guestbook/src/clj/guestbook/db/core.clj
@@ -45,41 +45,40 @@
 
 ; queries
 ; -------------
-(defn save-message!
-  ([params] (save-message! *db* params))
-  ([conn params]
-   (sql/insert! conn
-                :posts
-                (select-keys params [:name :message :author])
-                {:builder-fn rs/as-unqualified-maps})))
+(defn db-query [conn sql]
+  (sql/query conn sql {:builder-fn rs/as-unqualified-maps}))
+
+(defn db-insert! [conn table-key kvs]
+  sql/insert! conn table-key kvs {:builder-fn rs/as-unqualified-maps})
 
 (defn get-messages
   ([] (get-messages *db*))
-  ([conn]
-   (sql/query conn
-              ["SELECT * FROM posts"]
-              {:builder-fn rs/as-unqualified-maps})))
-
-(defn create-user!
-  ([params] (create-user! *db* params))
-  ([conn params]
-   (sql/insert! conn
-                :users
-                (select-keys params [:login :password])
-                {:builder-fn rs/as-unqualified-maps})))
+  ([conn] (db-query conn
+                    ["SELECT * FROM posts"])))
 
 (defn get-user-for-auth
   ([params] (get-user-for-auth *db* params))
   ([conn {:keys [login]}]
-   (first (sql/query conn
-                     ["SELECT * FROM users WHERE login = ?" login]
-                     {:builder-fn rs/as-unqualified-maps}))))
+   (first (db-query conn
+                    ["SELECT * FROM users WHERE login = ?" login]))))
 
-; one-off migration of data from h2 to postgres
-; Note: using jdbc here as opposed to next.jdbc so format of datasource is different
-; ---------
+(defn create-user!
+  ([params] (create-user! *db* params))
+  ([conn params]
+   (db-insert! conn
+               :users
+               (select-keys params [:login :password]))))
 
+(defn save-message!
+  ([params] (save-message! *db* params))
+  ([conn params]
+   (db-insert! conn
+               :posts
+               (select-keys params [:name :message :author]))))
 
+;; one-off migration of data from h2 to postgres
+;; Note: using jdbc here as opposed to next.jdbc so format of datasource is different
+;; ---------
 (comment
   (->>
    (jdbc/query
