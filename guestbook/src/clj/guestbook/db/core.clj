4585ea25dc5849f9d423ac9e7e5c1f59df3c5772
Joe Averbukh <joeaverbukh@gmail.com>
2020-12-16 23:03:21 -0500

    cleanup

--- guestbook/src/clj/guestbook/db/core.clj
+++ guestbook/src/clj/guestbook/db/core.clj
@@ -1,20 +1,23 @@
 (ns guestbook.db.core
   (:require
    [clojure.tools.logging :as log]
-   [clojure.java.jdbc :as jdbc]
    [cheshire.core :refer [generate-string parse-string]]
    [next.jdbc :as next-jdbc]
    [next.jdbc.sql :as sql]
+   [next.jdbc.prepare :as p]
    [next.jdbc.result-set :as rs]
    [conman.core :as conman]
-   [java-time.pre-java8 :as jt]
    [mount.core :refer [defstate]]
 
    [guestbook.config :refer [env]])
-  (:import org.postgresql.util.PGobject
-           java.sql.Array
-           clojure.lang.IPersistentMap
-           clojure.lang.IPersistentVector))
+  (:import (org.postgresql.util PGobject)
+           (java.sql PreparedStatement)
+           (java.time
+            Instant
+            LocalDate
+            LocalDateTime)
+           (java.sql Array)
+           (clojure.lang IPersistentMap)))
 
 (defstate ^:dynamic *db*
   :start (conman/connect! {:jdbc-url (env :database-url)})
@@ -52,47 +55,38 @@
   (read-column-by-label [^PGobject v _] (<-pgobject v))
   (read-column-by-index [^PGobject v _2 _3] (<-pgobject v)))
 
-(extend-protocol jdbc/ISQLValue
-  java.util.Date
-  (sql-value [v]
-    (java.sql.Timestamp. (.getTime v)))
-  java.time.LocalTime
-  (sql-value [v]
-    (jt/sql-time v))
-  java.time.LocalDate
-  (sql-value [v]
-    (jt/sql-date v))
-  java.time.LocalDateTime
-  (sql-value [v]
-    (jt/sql-timestamp v))
-  java.time.ZonedDateTime
-  (sql-value [v]
-    (jt/sql-timestamp v))
+(extend-protocol p/SettableParameter
+  Instant
+  (set-parameter [^java.time.Instant v ^PreparedStatement ps ^long i]
+    (.setTimestamp ps i (java.sql.Timestamp/from v)))
+
+  LocalDate
+  (set-parameter [^java.time.LocalDate v ^PreparedStatement ps ^long i]
+    (.setTimestamp ps i (java.sql.Timestamp/valueOf (.atStartOfDay v))))
+
+  LocalDateTime
+  (set-parameter [^java.time.LocalDateTime v ^PreparedStatement ps ^long i]
+    (.setTimestamp ps i (java.sql.Timestamp/valueOf v)))
+
   IPersistentMap
-  (sql-value [v]
-    (->pgobject v))
-  IPersistentVector
-  (sql-value [v]
-    (->pgobject v)))
+  (set-parameter [m ^PreparedStatement s i]
+    (.setObject s i (->pgobject m))))
+
 
 ; queries
 ; -------------
+
+
 (defn db-select [conn query]
   (log/info "Running select: " query)
   (sql/query conn query {:builder-fn rs/as-unqualified-maps}))
 
 (def db-select-one (comp first db-select))
 
-(defn db-insert! [conn query]
-  (log/info "Running insert: " query)
+(defn db-execute-one! [conn query]
+  (log/info "Running query: " query)
   (next-jdbc/execute-one! conn query {:builder-fn rs/as-unqualified-maps
                                       :return-keys true}))
-
-(defn db-update! [conn query]
-  (log/info "Running update: " query)
-  (next-jdbc/execute-one! conn query {:builder-fn rs/as-unqualified-maps
-                                      :return-keys true}))
-
 (defn get-messages
   "Grabs all posts"
   ([] (get-messages *db*))
@@ -122,29 +116,19 @@
 (defn create-user!
   ([params] (create-user! *db* params))
   ([conn {:keys [login password]}]
-   (db-insert! conn
-               ["INSERT INTO users (login,password) VALUES (?,?)"
-                login password])))
+   (db-execute-one! conn
+                    ["INSERT INTO users (login,password) VALUES (?,?)"
+                     login password])))
 
 (defn save-message!
   ([params] (save-message! *db* params))
   ([conn {:keys [name message author]}]
-   (db-insert! conn
-               ["INSERT INTO posts (name,message,author) VALUES (?,?,?)"
-                name message author])))
+   (db-execute-one! conn
+                    ["INSERT INTO posts (name,message,author) VALUES (?,?,?)"
+                     name message author])))
 
 (defn set-profile-for-user!
   ([params] (set-profile-for-user! *db* params))
   ([conn {:keys [profile login]}]
-   (db-update! conn ["UPDATE users SET profile = ?::jsonb WHERE login = ?"
-                     (->json profile) login])))
-
-;; one-off migration of data from h2 to postgres
-;; Note: using jdbc here as opposed to next.jdbc so format of datasource is different
-;; ---------
-(comment
-  (->>
-   (jdbc/query
-    {:connection-uri "jdbc:h2:./guestbook_dev.db"}
-    ["select name, message, timestamp from guestbook"])
-   (jdbc/insert-multi! {:connection-uri (str "jdbc:" (env :database-url))} :posts)))
+   (db-execute-one! conn ["UPDATE users SET profile = ?::jsonb WHERE login = ?"
+                          profile login])))
